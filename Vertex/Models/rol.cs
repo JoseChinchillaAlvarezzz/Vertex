56301db1026a74a6bf1c6811e949bbf5a9f3d0f2
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-05-28 20:37:05 -0600

    xddd

--- Vertex/Models/rol.cs
+++ Vertex/Models/rol.cs
@@ -1,6 +1,11 @@
-﻿namespace Vertex.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vertex.Models
 {
     public class rol
     {
+        [Key]
+        public int id { get; set; }
+        public string rol { get; set; }
     }
 }
