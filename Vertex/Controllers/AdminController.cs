fde0dddf889827b9bd46c29ad13a70196f799e16
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-06-03 00:04:36 -0600

    Admin a medias

--- Vertex/Controllers/AdminController.cs
+++ Vertex/Controllers/AdminController.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Vertex.Models;
 
 namespace Vertex.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ticketsContext _context;
+        public AdminController(ticketsContext context) 
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var adminId = HttpContext.Session.GetInt32("usuarioId");
+            var nombre = HttpContext.Session.GetString("nombre");
+
+            ViewBag.Bienvenida = "Bienvenido " + nombre;
+
+            var listadoTicketsPendientes = (from t in _context.tickets
+                                            join p in _context.prioridades on t.prioridad_id equals p.id
+                                            where t.usuario_id == adminId
+                                            select new
+                                            {
+                                                id = t.id,
+                                                prioridad = p.prioridad
+                                            }).ToList();
+
+            ViewData["listadoTickets"] = listadoTicketsPendientes;
+
             return View();
         }
     }
