fa1f1931b28e7390efee4f99bb74e36c24b2abc1
Gabo <Gabri@GABO>
2025-06-04 11:43:02 -0600

    hol

--- Vertex/Controllers/TecnicoController.cs
+++ Vertex/Controllers/TecnicoController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Vertex.Models;
-using Microsoft.EntityFrameworkCore;
-using Vertex.Models;
 using System.Linq;
 
 namespace Vertex.Controllers
@@ -21,81 +19,10 @@ namespace Vertex.Controllers
         }
 
         public IActionResult Tickets()
-            var nombreUsuario = HttpContext.Session.GetString("nombre");
-            ViewBag.NombreUsuario = nombreUsuario;
-
-            int? tecnicoId = HttpContext.Session.GetInt32("usuarioId");
-
-            List<tickets> listaTickets = new();
-
-            if (tecnicoId != null)
-            {
-                listaTickets = _context.asignaciones
-                    .Where(a => a.usuario_id == tecnicoId)
-                    .Select(a => a.ticket_id)
-                    .Distinct()
-                    .Join(_context.tickets,
-                        id => id,
-                        t => t.id,
-                        (id,t) => t)
-                    .Where(t => t.estado_ticket_id == 1)
-                    .ToList();
-            }
-
-            return View("Index", listaTickets);
-
-        }
-        public IActionResult MisAsignaciones()
-        {
-            var nombreUsuario = HttpContext.Session.GetString("nombre");
-            ViewBag.NombreUsuario = nombreUsuario;
-
-            int? tecnicoId = HttpContext.Session.GetInt32("usuarioId");
-
-            List<tickets> listaTickets = new();
-
-            if (tecnicoId != null)
-            {
-                listaTickets = _context.asignaciones
-                    .Where(a => a.usuario_id == tecnicoId)
-                    .Select(a => a.ticket_id)
-                    .Distinct()
-                    .Join(_context.tickets,
-                          id => id,
-                          t => t.id,
-                          (id, t) => t)
-                    .Where(t => t.estado_ticket_id == 1) // Pendientes
-                    .ToList();
-            }
-
-            return View("MisAsignaciones", listaTickets);
-        }
-
-        public IActionResult Detalle(int id)
         {
             // Trae todos los tickets, puedes filtrar por técnico si lo deseas
             var listadoTickets = _context.tickets.ToList();
             return View(listadoTickets);
-            // 1. Buscar el ticket
-            var ticket = _context.tickets.FirstOrDefault(t => t.id == id);
-            if (ticket == null)
-                return NotFound();
-
-            // 2. Cargar manualmente los datos relacionados
-            var categoria = _context.categorias.FirstOrDefault(c => c.id == ticket.categoria_id);
-            var prioridad = _context.prioridades.FirstOrDefault(p => p.id == ticket.prioridad_id);
-            var estado = _context.estados_tickets.FirstOrDefault(e => e.id == ticket.estado_ticket_id);
-            var cliente = _context.clientes.FirstOrDefault(c => c.id == ticket.cliente_id);
-
-            // 3. Enviar todo por ViewBag como hiciste con nombreUsuario
-            ViewBag.Categoria = categoria?.categoria ?? "Sin categoría";
-            ViewBag.Prioridad = prioridad?.prioridad ?? "Sin prioridad";
-            ViewBag.Estado = estado?.estado ?? "Sin estado";
-            ViewBag.Cliente = cliente != null ? $"{cliente.nombre} {cliente.apellido}" : "Sin cliente";
-
-            return View("Detalle", ticket);
-
         }
-
     }
 }
